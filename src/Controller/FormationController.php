ed2a5a1eaa6cd1e0a3cbb1b386297c086092f254
Vincent <vincentmaury20@gmail.com>
2025-10-27 15:50:15 +0100

    feat home: static homepage setup, basic styling, photo import + page creation option

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
-use App\Form\FormationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,44 +21,13 @@ final class FormationController extends AbstractController
         ]);
     }
 
-    //     #[Route('/formation/{id}/edit', name: 'app_formation_edit')]
-    //     public function edit(Request $request, Formation $formation, EntityManagerInterface $em): Response
-    //     {
-    //         $form = $this->createForm(FormationType::class, $formation);
-    //         $form->handleRequest($request);
+    #[Route('/formation/{id}', name: 'app_formation_show')]
+    public function show(int $id, EntityManagerInterface $em): Response
+    {
+        $formation = $em->getRepository(Formation::class)->findById($id);
 
-    //         if ($form->isSubmitted() && $form->isValid()) {
-    //             $em->persist($formation);
-    //             $em->flush();
-
-    //             $this->addFlash('success', 'Formation mise à jour.');
-
-    //             return $this->redirectToRoute('app_formation');
-    //         }
-
-    //         return $this->render('formation/edit.html.twig', [
-    //             'form' => $form->createView(),
-    //             'formation' => $formation,
-    //         ]);
-    //     }
-
-    //     #[Route('/formation/new', name: 'app_formation_new')]
-    //     public function create(Request $request, EntityManagerInterface $em): Response
-    //     {
-    //         $formation = new Formation();
-    //         $form = $this->createForm(FormationType::class, $formation);
-    //         $form->handleRequest($request);
-
-    //         if ($form->isSubmitted() && $form->isValid()) {
-    //             dump($formation);
-    //             $em->persist($formation);
-    //             $em->flush();
-    //             $this->addFlash('success', 'Formation créée.');
-    //             return $this->redirectToRoute('app_formation');
-    //         }
-
-    //         return $this->render('formation/new.html.twig', [
-    //             'form' => $form->createView(),
-    //         ]);
-    //     }
+        return $this->render('formation/show.html.twig', [
+            'formation' => $formation,
+        ]);
+    }
 }
